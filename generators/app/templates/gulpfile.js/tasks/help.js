0640bf5ec4948af02292152a04714a987f80c881
Jay <Pyramidium@192.168.1.101>
2015-09-20 23:42:26 +0100

    Update gulp help task

--- generators/app/templates/gulpfile.js/tasks/help.js
+++ generators/app/templates/gulpfile.js/tasks/help.js
@@ -4,13 +4,9 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 
 gulp.task('help', function() {
-  gutil.log(gutil.colors.red('bannertime'), 'help');
+  gutil.log('There are 2 basic commands');
   gutil.log('--------------------------');
-  gutil.log('There are 3 basic commands.');
-  gutil.log(gutil.colors.yellow('gulp'), ': for dev use, spins up server w livereload as you edit files');
-  gutil.log(gutil.colors.yellow('gulp build'), ': minifies files from the dev directory into the', gutil.colors.red('dist'), 'directory');
-  gutil.log('and creates a zip of these files in', gutil.colors.red('delivery'), 'directory');
-  gutil.log(gutil.colors.yellow('gulp archive'), 'takes files from the '+ gutil.colors.red('dev'), 'directory' + ' plus other important files');
-  gutil.log('and zips them in the', gutil.colors.red('archive'), 'directory for archival purposes.');
+  gutil.log(gutil.colors.green('gulp'), 'compiles source files and fires up dev server');
+  gutil.log(gutil.colors.green('gulp prod'), 'prepares files for production');
   gutil.log('--------------------------');
 });
