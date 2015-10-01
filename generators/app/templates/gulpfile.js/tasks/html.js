1a38d587cd8a45ad92ef0f4ce505e6ad8acd47b4
Jay <jay@pyramidium.co.uk>
2015-10-01 15:29:20 +0100

    Remove usemin

--- generators/app/templates/gulpfile.js/tasks/html.js
+++ generators/app/templates/gulpfile.js/tasks/html.js
@@ -6,12 +6,10 @@ var gulp = require('gulp');
 var gulpif = require('gulp-if');
 var htmlmin = require('gulp-htmlmin');
 var uglify = require('gulp-uglify');
-var usemin = require('gulp-usemin');
 
 gulp.task('html', function() {
   return gulp.src(config.src)
     .pipe(gulpif(process.env.NODE_ENV == 'production', htmlmin(config.htmlmin)))
-    .pipe(gulpif(process.env.NODE_ENV == 'production', usemin({ js: [ uglify() ] }), usemin()))
     .pipe(gulp.dest(config.dest))
     .pipe(browserSync.reload({stream: true}));
 });
