6831919df2001eb54dd8204a525542838b728e0c
Jay <jay@pyramidium.co.uk>
2015-11-30 17:46:17 +0000

    feat: iterate through public folders to create zips closes #15

--- generators/app/templates/gulpfile.js/tasks/zip.js
+++ generators/app/templates/gulpfile.js/tasks/zip.js
@@ -1,14 +1,28 @@
 'use strict';
 
-var config = require('../config/zip');
+var config = require('../config');
+var configZip = require('../config/zip');
 var del = require('del');
 var filesize = require('gulp-filesize');
+var fs = require('fs');
 var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var path = require('path');
 var zip = require('gulp-zip');
 
 gulp.task('zip', function() {
-  return gulp.src(config.src)
-    .pipe(zip(config.archiveName))
-    .pipe(filesize())
-    .pipe(gulp.dest(config.dest));
+  function getFolders(dir) {
+    return fs.readdirSync(dir)
+      .filter(function(file) {
+        return fs.statSync(path.join(dir, file)).isDirectory();
+      });
+  }
+  var folders = getFolders(config.publicDirectory);
+  var tasks = folders.map(function(folder) {
+    return gulp.src(path.join(config.publicDirectory, folder, '/*'))
+      .pipe(gulpif(folder != 'base', zip(folder + '.zip')))
+      .pipe(gulpif(folder != 'base', filesize()))
+      .pipe(gulpif(folder != 'base', gulp.dest(configZip.dest)));
+  });
+  return tasks;
 });
