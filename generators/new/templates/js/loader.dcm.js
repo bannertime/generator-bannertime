03956013b09df2fd5df82e6b28efd3449fae639b
pyramidium <jay@pyramidium.co.uk>
2015-12-19 14:45:11 +0000

    fix: dcm loader jshint errors

--- generators/new/templates/js/loader.dcm.js
+++ generators/new/templates/js/loader.dcm.js
@@ -43,11 +43,11 @@ Banner.prototype.politeLoad = function(urls, onComplete) {
  * Load script method.
  */
 Banner.prototype.loadScript = function(url, callback) {
-  var script = document.createElement('script')
+  var script = document.createElement('script');
   script.type = 'text/javascript';
   if (script.readyState) {
     script.onreadystatechange = function() {
-      if (script.readyState == 'loaded' || script.readyState == 'complete') {
+      if (script.readyState === 'loaded' || script.readyState === 'complete') {
         script.onreadystatechange = null;
         callback();
       }
@@ -65,10 +65,10 @@ Banner.prototype.loadScript = function(url, callback) {
  * Bind Enabler events.
  */
 Banner.prototype.bindEvents = function() {
-  this.banner.addEventListener('click', function(e) {
+  this.banner.addEventListener('click', function() {
     window.open(window.clickTag);
   });
-  this.banner.addEventListener('touchend', function(e) {
+  this.banner.addEventListener('touchend', function() {
     window.open(window.clickTag);
   });
 };
