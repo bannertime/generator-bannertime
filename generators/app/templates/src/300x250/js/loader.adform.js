8fd58255f6b6e67511924facae4f62e6274e696e
Joey Padasian <joey@padasian.com>
2016-01-03 20:42:18 +0000

    fix: adform loader jshint errors

--- generators/app/templates/src/300x250/js/loader.adform.js
+++ generators/app/templates/src/300x250/js/loader.adform.js
@@ -43,11 +43,11 @@ Banner.prototype.politeLoad = function(urls, onComplete) {
  * Load script method.
  */
 Banner.prototype.loadScript = function(url, callback) {
-  var script = document.createElement('script')
+  var script = document.createElement('script');
   script.type = 'text/javascript';
   if (script.readyState) {
     script.onreadystatechange = function() {
-      if (script.readyState == 'loaded' || script.readyState == 'complete') {
+      if (script.readyState === 'loaded' || script.readyState === 'complete') {
         script.onreadystatechange = null;
         callback();
       }
@@ -67,10 +67,10 @@ Banner.prototype.loadScript = function(url, callback) {
 Banner.prototype.bindEvents = function() {
   var clickTAGvalue = dhtml.getVar('clickTAG', '//www.adform.com');
   var landingPageTarget = dhtml.getVar('landingPageTarget', '_blank');
-  this.banner.addEventListener('click', function(e) {
+  this.banner.addEventListener('click', function() {
     window.open(clickTAGvalue, landingPageTarget);
   });
-  this.banner.addEventListener('touchend', function(e) {
+  this.banner.addEventListener('touchend', function() {
     window.open(clickTAGvalue, landingPageTarget);
   });
 };
