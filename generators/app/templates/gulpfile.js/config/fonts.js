53b53c6d839c4ef9fdcad23810ab67eb493d6448
Jay <jay@pyramidium.co.uk>
2016-01-04 12:34:48 +0000

    chore: convert config files into one single config resolves #23

--- generators/app/templates/gulpfile.js/config/fonts.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var config = require('./');
-
-module.exports = {
-  watch: config.sourceAssets + '/**/fonts/**/*',
-  src: config.sourceAssets + '/**/fonts/**/*',
-  dest: config.publicAssets
-};
