b5f7eab0b5095d3997ef8a51bc3b885788a63c27
Jay <jay@pyramidium.co.uk>
2016-02-10 12:29:57 +0000

    fix: new generator should use app templates resolves #40

--- generators/new/modules/files.js
+++ generators/new/modules/files.js
@@ -40,12 +40,12 @@ module.exports = function(Bannertime) {
   Bannertime.prototype.previewHtml = function() {
     var filePath = './src/index.html';
     var indexOptions = {
-      title: this.props.bannerName,
+      bannerName: this.props.bannerName,
     };
     this.fs.copy(filePath, filePath, {
       process: function(content) {
         var regEx = new RegExp('</ul>');
-        var newContent = content.toString().replace(regEx, '  <li><a href="' + indexOptions.title + '/" class="done">' + indexOptions.title + '</a></li>\n      </ul>');
+        var newContent = content.toString().replace(regEx, '  <li><a href="' + indexOptions.bannerName + '/" class="done">' + indexOptions.bannerName + '</a></li>\n      </ul>');
         return newContent;
       }
     });
@@ -56,14 +56,14 @@ module.exports = function(Bannertime) {
    */
   Bannertime.prototype.html = function() {
     var indexOptions = {
-      title: this.props.bannerName,
+      bannerName: this.props.bannerName,
       bannerType: this.props.bannerType,
       bannerWidth: this.props.bannerWidth,
       bannerHeight: this.props.bannerHeight,
       includeOfflineEnabler: this.props.includeOfflineScripts
     };
     this.fs.copyTpl(
-      this.templatePath('_index.html'),
+      this.templatePath('../../app/templates/src/300x250/_index.html'),
       this.destinationPath('src/' + this.props.bannerName + '/index.html'),
       indexOptions
     );
@@ -78,17 +78,24 @@ module.exports = function(Bannertime) {
       bannerHeight: this.props.bannerHeight
     };
     this.fs.copyTpl(
-      this.templatePath('styles/style.scss'),
+      this.templatePath('../../app/templates/src/300x250/styles/style.scss'),
       this.destinationPath('src/' + this.props.bannerName + '/styles/style.scss'),
       styleOptions
     );
+    this.fs.copy(
+      this.templatePath('../../app/templates/src/300x250/styles/base/banner.scss'),
+      this.destinationPath('src/' + this.props.bannerName + '/styles/base/_banner.scss'), {
+      process: function(content) {
+        var regExWidth = new RegExp(/\$banner-width:\s*[0-9]*px;/);
+        var regExHeight = new RegExp(/\$banner-height:\s*[0-9]*px;/);
+        var newContent = content.toString()
+          .replace(regExWidth, '$banner-width: ' + styleOptions.bannerWidth + 'px;')
+          .replace(regExHeight, '$banner-height: ' + styleOptions.bannerHeight + 'px;');
+        return newContent;
+      }
+    });
     this.fs.copyTpl(
-      this.templatePath('styles/base/_banner.scss'),
-      this.destinationPath('src/' + this.props.bannerName + '/styles/base/_banner.scss'),
-      styleOptions
-    );
-    this.fs.copyTpl(
-      this.templatePath('styles/base/preloader.scss'),
+      this.templatePath('../../app/templates/src/300x250/styles/base/preloader.scss'),
       this.destinationPath('src/' + this.props.bannerName + '/styles/base/_preloader.scss'),
       styleOptions
     );
@@ -100,26 +107,26 @@ module.exports = function(Bannertime) {
   Bannertime.prototype.js = function() {
     if (this.props.bannerType == 'Sizmek') {
       this.fs.copy(
-        this.templatePath('js/libs/EBLoader.js'),
+        this.templatePath('../../app/templates/src/300x250/js/libs/EBLoader.js'),
         this.destinationPath('src/' + this.props.bannerName + '/js/EBLoader.js')
       );
     }
     if (this.props.bannerType == 'Adform') {
       this.fs.copy(
-        this.templatePath('js/libs/AdformDHTML.js'),
+        this.templatePath('../../app/templates/src/300x250/js/libs/AdformDHTML.js'),
         this.destinationPath('src/' + this.props.bannerName + '/js/AdformDHTML.js')
       );
     }
     this.fs.copy(
-      this.templatePath('js/banner.js'),
+      this.templatePath('../../app/templates/src/300x250/js/banner.js'),
       this.destinationPath('src/' + this.props.bannerName + '/js/banner.js')
     );
     this.fs.copy(
-      this.templatePath('js/loader.' + this.bannerSuffix + '.js'),
+      this.templatePath('../../app/templates/src/300x250/js/loader.' + this.bannerSuffix + '.js'),
       this.destinationPath('src/' + this.props.bannerName + '/js/banner.loader.js')
     );
     this.fs.copy(
-      this.templatePath('js/animation.js'),
+      this.templatePath('../../app/templates/src/300x250/js/animation.js'),
       this.destinationPath('src/' + this.props.bannerName + '/js/banner.animation.js')
     );
   };
@@ -136,7 +143,7 @@ module.exports = function(Bannertime) {
         bannerHeight: this.props.bannerHeight,
       };
       this.fs.copyTpl(
-        this.templatePath('js/_manifest.json'),
+        this.templatePath('../../app/templates/src/300x250/js/_manifest.json'),
         this.destinationPath('src/' + this.props.bannerName + '/manifest.json'),
         manifestOptions
       );
@@ -147,21 +154,7 @@ module.exports = function(Bannertime) {
    * Process the banner images.
    */
   Bannertime.prototype.images = function() {
-    this.directory('images', 'src/' + this.props.bannerName + '/images');
-  };
-
-  /**
-   * Process the zip config.
-   */
-  Bannertime.prototype.zip = function() {
-    // var zipOptions = {
-    //   archiveName: this.props.archiveName
-    // };
-    // this.fs.copyTpl(
-    //   this.templatePath('_zip.js'),
-    //   this.destinationPath('gulpfile.js/config/zip.js'),
-    //   zipOptions
-    // );
+    this.directory('../../app/templates/src/300x250/images', 'src/' + this.props.bannerName + '/images');
   };
 
   /**
