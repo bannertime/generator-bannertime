90d71e5f50e23152450868ae907acec03bdde75c
pyramidium <jay@pyramidium.co.uk>
2016-02-18 03:51:06 +0000

    fix: babel register

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,7 +2,7 @@
 
 // Register the Babel require hook
 require('babel-core/register')({
-  only: /generator-test\/(?!node_modules)/
+  only: /generator-bannertime\/(?!node_modules)/
 });
 
 // Export the generator
