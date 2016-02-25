525b47f5034e5f6dcd5638fa7dab808618ee0935
pyramidium <jay@pyramidium.co.uk>
2016-02-25 22:03:05 +0000

    feat: add babel support to js gulp task

--- generators/app/templates/gulpfile.js/tasks/javascript.js
+++ generators/app/templates/gulpfile.js/tasks/javascript.js
@@ -1,16 +1,21 @@
 'use strict';
 
+var babel = require('gulp-babel');
 var browserSync  = require('browser-sync');
 var config = require('../config');
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
 var gulpif = require('gulp-if');
+var jshint = require('gulp-jshint');
+var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 
 gulp.task('js', function() {
   return gulp.src(config.tasks.js.src)
+    .pipe(sourcemaps.init())
     .pipe(gulpif(process.env.NODE_ENV == 'production', jshint()))
     .pipe(gulpif(process.env.NODE_ENV == 'production', jshint.reporter('default')))
+    .pipe(babel({presets: ['es2015']}))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulpif(process.env.NODE_ENV == 'production', uglify()))
     .pipe(gulp.dest(config.tasks.js.dest))
     .pipe(browserSync.reload({stream: true}));
