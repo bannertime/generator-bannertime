8169e6c3c51f14251677408522b995b16b922c27
pyramidium <jay@pyramidium.co.uk>
2016-02-29 17:44:33 +0000

    test: add test directory and run app

--- test/test-app.js
+++ test/test-app.js
@@ -1,16 +1,23 @@
+/*global describe, it, beforeEach */
 'use strict';
 
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 var path = require('path');
 
-describe('bannertime generator', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({
-        features: []
-      })
-      .on('end', done);
+describe('Bannertime Generator', function () {
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+      if (err) {
+        return done(err);
+      }
+      this.app = helpers.createGenerator(
+        'bannertime:app', [
+          '../../generators/app'
+        ]);
+
+      done();
+    }.bind(this));
   });
 
   it('the generator can be required without throwing', function () {
@@ -18,18 +25,35 @@ describe('bannertime generator', function () {
   });
 
   it('creates expected files', function () {
-    assert.file([
-      'src/base/images/desktop.png',
-      'src/base/images/loading.gif',
-      'src/base/images/logo.png',
-      'src/base/js/main.js',
-      'src/base/styles/style.scss',
-      'src/index.html',
-      '.editorconfig',
-      '.gitignore',
-      '.jshintrc',
-      'package.json',
-      'README.md',
-    ]);
+    helpers.mockPrompt(this.app, {
+      bannerName: '300x250',
+    });
+
+    this.app.options['skip-install'] = false;
+
+    this.app.run({}, function () {
+      assert.file([
+        'src/300x250/index.html',
+        'src/300x250/images/logo.png',
+        'src/300x250/js/banner.js',
+        'src/300x250/js/banner.loader.js',
+        'src/300x250/js/banner.animation.js',
+        'src/300x250/styles/style.scss',
+        'src/300x250/styles/base/_banner.scss',
+        'src/300x250/styles/base/_preloader.scss',
+        'src/base/images/desktop.png',
+        'src/base/images/loading.gif',
+        'src/base/images/logo.png',
+        'src/base/js/main.js',
+        'src/base/styles/style.scss',
+        'src/index.html',
+        '.editorconfig',
+        '.gitignore',
+        '.jshintrc',
+        'package.json',
+        'README.md',
+      ]);
+      done();
+    });
   });
 });
