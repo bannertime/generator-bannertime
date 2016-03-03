ef96ab906dca15c66c31fda314e312b332c32c27
Jay Campbell <jay@pyramidium.co.uk>
2016-03-03 14:59:36 +0000

    fix: sourcemaps break uglification when using gulp prod resolves #48

--- generators/app/templates/gulpfile.js/tasks/javascript.js
+++ generators/app/templates/gulpfile.js/tasks/javascript.js
@@ -6,16 +6,13 @@ var config = require('../config');
 var gulp = require('gulp');
 var gulpif = require('gulp-if');
 var jshint = require('gulp-jshint');
-var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 
 gulp.task('js', function() {
   return gulp.src(config.tasks.js.src)
-    .pipe(sourcemaps.init())
     .pipe(gulpif(process.env.NODE_ENV == 'production', jshint()))
     .pipe(gulpif(process.env.NODE_ENV == 'production', jshint.reporter('default')))
     .pipe(babel({presets: ['es2015']}))
-    .pipe(sourcemaps.write('.'))
     .pipe(gulpif(process.env.NODE_ENV == 'production', uglify()))
     .pipe(gulp.dest(config.tasks.js.dest))
     .pipe(browserSync.reload({stream: true}));
