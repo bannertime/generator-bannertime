be90248580818a6bbda9a8ca9787df0da1db85d4
pyramidium <jay@pyramidium.co.uk>
2016-03-12 00:41:25 +0000

    chore: rename generators to src

--- generators/new/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Register the Babel require hook
-require('babel-core/register')({
-  only: /generator-bannertime\/(?!node_modules)/
-});
-
-// Export the generator
-exports = module.exports = require('./generator');
