85fdd9723770bd4ff05b9c501fb334531cf19451
pyramidium <jay@pyramidium.co.uk>
2016-03-22 12:01:08 +0000

    chore: replace gulp-shell with execSync resolves #53

--- src/deploy/index.js
+++ src/deploy/index.js
@@ -4,7 +4,6 @@ import {Base} from 'yeoman-generator';
 import intro from './modules/intro';
 import prompts from './modules/prompts';
 import files from './modules/files';
-import install from './modules/install';
 
 export default class Generator extends Base {
   constructor(...args) {
@@ -15,4 +14,3 @@ export default class Generator extends Base {
 Generator.prototype.intro = intro;
 Generator.prototype.prompts = prompts;
 Generator.prototype.files = files;
-Generator.prototype.install = install;
--- src/deploy/templates/deploy.js
+++ src/deploy/templates/deploy.js
@@ -2,11 +2,8 @@
 
 var gulp  = require('gulp');
 var pkg = require('../../package.json');
-var shell = require('gulp-shell');
+var execSync = require('child_process').execSync;
 
 gulp.task('deploy', ['prod'], function() {
-  return gulp.src('*.js', {read: false})
-    .pipe(shell(['bash gulpfile.js/lib/deploy.sh ' + pkg.name], {
-      interactive: true
-    }));
+  execSync(['bash gulpfile.js/lib/deploy.sh ' + pkg.name], { stdio: 'inherit' });
 });
