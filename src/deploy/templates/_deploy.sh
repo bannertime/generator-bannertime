1b8533a08dd6b27f4485f0541043b6217f270c92
pyramidium <jay@pyramidium.co.uk>
2016-04-14 15:32:45 +0100

    refactor: deploy script to allow branch based deployment

--- src/deploy/templates/_deploy.sh
+++ src/deploy/templates/_deploy.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-# Go into the public facing directory and create a *new* Git repo
 
-PROJECT="$1"
 DIRECTORY=public
 USER=<%= deployUsername %>
 DOMAIN=<%= deployDomain %>
+BRANCH="$(git symbolic-ref --short -q HEAD)"
+
+if [ "$BRANCH" = "master" ]
+then
+  PROJECT="$1"
+else
+  PROJECT="$1-$BRANCH"
+  echo "On $BRANCH branch, deploying to $PROJECT"
+fi
 
 if [ -d "$DIRECTORY" ];
 then
-  cd public
+  cd $DIRECTORY
   ssh $USER@$DOMAIN "mkdir -p htdocs/$PROJECT" && scp -r . $USER@$DOMAIN:htdocs/$PROJECT/
   open http://$DOMAIN/$PROJECT/
 else
-  echo "Public directory does not exist!"
+  echo "$DIRECTORY does not exist!"`
 fi
