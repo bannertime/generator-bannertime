86c5694a7d82623541d1041c5935febea9a31427
pyramidium <jay@pyramidium.co.uk>
2016-05-09 18:42:21 +0100

    refactor: retina image and scale animation

--- src/app/templates/src/300x250/js/animate.js
+++ src/app/templates/src/300x250/js/animate.js
@@ -1,5 +1,5 @@
 export default function () {
   this.timeline = new TimelineMax({ repeat: -1 })
-    .to(this.logo.element, 2, { autoAlpha: 1, rotation: 360, delay: 1, ease: Elastic.easeOut })
-    .to(this.logo.element, 1, { autoAlpha: 0, rotation: 0, delay: 1 });
+    .to(this.logo.element, 2, { autoAlpha: 1, scale: 0.7, delay: 1, ease: Elastic.easeOut })
+    .to(this.logo.element, 1, { autoAlpha: 0, scale: 0.4, delay: 1 });
 }
--- src/app/templates/src/300x250/js/createElements.js
+++ src/app/templates/src/300x250/js/createElements.js
@@ -3,6 +3,7 @@ import SmartObject from '../../modules/SmartObject';
 export default function () {
   this.logo = new SmartObject({
     backgroundImage: 'images/logo.png',
+    retina: true,
     parent: this.banner
   });
 }
