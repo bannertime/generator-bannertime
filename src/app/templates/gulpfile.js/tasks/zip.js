8708a658b0af6dc8e741e607811df4d1d2b7f747
Joey Padasian <joey.padasian@saatchi.co.uk>
2016-10-19 12:45:23 +0100

    feat: CL argument available to remove backup images from production zips closes #64

--- src/app/templates/gulpfile.js/tasks/zip.js
+++ src/app/templates/gulpfile.js/tasks/zip.js
@@ -7,6 +7,7 @@ var fs = require('fs');
 var gulp = require('gulp');
 var gulpif = require('gulp-if');
 var path = require('path');
+var rename = require('gulp-rename');
 var zip = require('gulp-zip');
 
 gulp.task('zip', function() {
@@ -16,12 +17,22 @@ gulp.task('zip', function() {
         return fs.statSync(path.join(dir, file)).isDirectory();
       });
   }
+  var separate = (process.argv.indexOf('--separate-backups') >= 0 || process.argv.indexOf('--sb') >= 0) ? true : false;
   var folders = getFolders(config.root.dest);
   var tasks = folders.map(function(folder) {
-    return gulp.src(path.join(config.root.dest, folder, '/**/*'))
-      .pipe(gulpif(folder != 'base', zip(folder + '.zip')))
-      .pipe(gulpif(folder != 'base', filesize()))
-      .pipe(gulpif(folder != 'base', gulp.dest(config.tasks.zip.dest)));
+    if (folder != 'base') {
+      return gulp.src(path.join(config.root.dest, folder, '/**/*'))
+      .pipe(gulpif(separate && 'images/backup.jpg',
+        rename(function (path) {
+          path.dirname = './';
+          path.basename = folder + '-backup';
+          return path;
+        })
+      ))
+      .pipe(gulpif(separate && '!' + folder + '-backup.jpg', zip(folder + '.zip')))
+      .pipe(filesize())
+      .pipe(gulp.dest(config.tasks.zip.dest));
+    }
   });
   return tasks;
 });
