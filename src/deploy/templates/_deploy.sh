cfe682eebc5d2bec90b2c603337f46c6ec59a58c
Joey Padasian <joey.padasian@saatchi.co.uk>
2016-10-26 10:50:38 +0100

    fix: check for git repository before deploying fixes #65

--- src/deploy/templates/_deploy.sh
+++ src/deploy/templates/_deploy.sh
@@ -3,7 +3,13 @@
 DIRECTORY=public
 USER=<%= deployUsername %>
 DOMAIN=<%= deployDomain %>
-BRANCH="$(git symbolic-ref --short -q HEAD)"
+
+if [ -d .git ]; then
+  BRANCH="$(git symbolic-ref --short -q HEAD)";
+else
+  echo -e "\033[1;33mwarning: \033[0mCurrent directory is not a git repository"
+  BRANCH="master"
+fi;
 
 if [ "$BRANCH" = "master" ]
 then
@@ -15,9 +21,10 @@ fi
 
 if [ -d "$DIRECTORY" ];
 then
+  echo "Uploading to server"
   cd $DIRECTORY
   ssh $USER@$DOMAIN "mkdir -p htdocs/$PROJECT" && scp -r . $USER@$DOMAIN:htdocs/$PROJECT/
   open http://$DOMAIN/$PROJECT/
 else
-  echo "$DIRECTORY does not exist!"
+  echo -e "\033[0;31merror: \033[0m$DIRECTORY directory does not exist!"
 fi
